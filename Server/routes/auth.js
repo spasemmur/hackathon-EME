fc68963377b7030035f07b35aa9e73582c6fdd10
spasemmur <spasemmur@gmail.com>
2026-07-02 16:32:19 +0300

    fdfdf

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
